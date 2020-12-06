8dcaf311c963ab13cbc758233c2d890e060f3193
unknown <bebekngayang@gmail.com>
2020-12-06 16:07:55 +0700

    Add template Inheritance

--- routing.js
+++ routing.js
@@ -6,19 +6,25 @@ const view = require('swig');
 
 //menambahkan router baru
 router.addRoute('/', (req, res) => {
-    let homepage = view.compileFile('./templates/index.html')();
+    let homepage = view.compileFile('./templates/index.html')({
+        title: "Homepage"
+    });
     res.writeHead(200, { "Content-type": "text/html" });
     res.end(homepage);
     console.log("access: homepage");
 });
 router.addRoute('/about', (req, res) => {
-    let about = view.compileFile('./templates/about.html')();
+    let about = view.compileFile('./templates/about.html')({
+        title: "About Page"
+    });
     res.writeHead(200, { "Content-type": "text/html" });
     res.end(about);
     console.log("access: about page");
 });
 router.addRoute('/contact', (req, res) => {
-    let contact = view.compileFile('./templates/contact.html')();
+    let contact = view.compileFile('./templates/contact.html')({
+        title: "Contact Page"
+    });
     res.writeHead(200, { "Content-type": "text/html" });
     res.end(contact);
     console.log("access: contact page");
@@ -31,7 +37,9 @@ const server = http.createServer((req, res) => {
         match.fn(req, res, match);
     } else {
         res.statusCode = 404;
-        let unknownPage = view.compileFile('./templates/404.html')();
+        let unknownPage = view.compileFile('./templates/404.html')({
+            title: "Error Page"
+        });
         res.writeHead(200, { "Content-type": "text/html" });
         res.end(unknownPage);
         console.log('this page is not exist');
