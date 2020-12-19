9f77deb1866c6cd890fa2cdc9f52afd97fa9fe10
unknown <bebekngayang@gmail.com>
2020-12-19 21:20:30 +0700

    adding mysql connect

--- routing.js
+++ routing.js
@@ -3,7 +3,14 @@ const url = require('url');
 const Router = require('routes');
 const router = new Router();
 const view = require('swig');
-
+const mysql = require('mysql');
+const dbconnect = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    database: "nodejs",
+    user: "admin",
+    password: "admin123"
+});
 //menambahkan router baru
 router.addRoute('/', (req, res) => {
     let homepage = view.compileFile('./templates/index.html')({
@@ -30,6 +37,50 @@ router.addRoute('/contact', (req, res) => {
     console.log("access: contact page");
 });
 
+router.addRoute('/profile', (req, res) => {
+    dbconnect.query('select * from profile', (err, rows, field) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/plain" });
+        res.end(JSON.stringify(rows));
+    });
+});
+
+router.addRoute('/insert', (req, res) => {
+    dbconnect.query('insert into profile set ?', {
+        id: "",
+        Name: 'Mahrus',
+        Address: 'Lethbridge, Alberta canada',
+        Telp: "+82001344567",
+        Email: "mahrus@gmail.com"
+    }, function(err, field) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/plain" });
+        res.end(`Added ${field.affectedRows} Affected Rows`);
+    });
+});
+
+router.addRoute('/update', (req, res) => {
+    dbconnect.query('update profile set ? where ?', [
+        { id: '' },
+        { Name: 'Aat' },
+        { Address: 'Aspen Grove, British Columbia Canada' },
+        { Telp: '+63001507363' },
+        {
+            Email: 'Aat18agustus@gmail.com'
+        }
+    ], function(err, field) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/plain" });
+        console.log(`${field.changedRows} Updated`);
+        res.end(`${field.changedRows} Updated`);
+    });
+});
+
+router.addRoute('/delete', (req, res) => {
+    res.writeHead(200, { "Content-type": "text/plain" });
+    res.end();
+});
+
 const server = http.createServer((req, res) => {
     let path = url.parse(req.url).pathname;
     let match = router.match(path);
@@ -42,7 +93,6 @@ const server = http.createServer((req, res) => {
         });
         res.writeHead(200, { "Content-type": "text/html" });
         res.end(unknownPage);
-        console.log('this page is not exist');
     }
 });
 server.listen(5000);
